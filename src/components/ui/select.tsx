980c3312c8792377b91ae4f7686cb00fc67d6be6
Яблоков Максим Вадимович <MYablokov@alfabank.ru>
2026-09-25 12:41:40 +0300

    fix: Не сужать меню выбора относительно кнопки
    - Сохранить ширину по самому длинному пункту с нижней границей по ширине кнопки выбора.

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -88,7 +88,7 @@ const SelectContent = React.forwardRef<
         className={cn(
           "p-1",
           position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-max max-w-full"
+            "h-[var(--radix-select-trigger-height)] w-max min-w-[var(--radix-select-trigger-width)] max-w-full"
         )}
       >
         {children}
